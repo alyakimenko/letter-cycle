a4a56a737a9e4dc947e9412b435019eb0d12e410
alyakimenko <urlucidfall@gmail.com>
2019-08-21 11:36:13 +0300

    define constants

---
+++ cmd/letter-cycle/main.go
@@ -0,0 +1,14 @@
+package main
+
+const (
+	letterNum = 26
+
+	fullRotation = 360.0
+	rotation     = 30.0 // The rotation angle
+	step         = .03  // Transparency step
+
+	width  = 200
+	height = 200
+)
+
+var transperency = 1.0
